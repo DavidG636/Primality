19fb96011948edc078901f437810a451dad4d286
David Gonzalez <gonzalezdavid636@gmail.com>
2018-11-10 15:10:44 -0500

    Add code to show user whether number inputted is prime or not

--- script.js
+++ script.js
@@ -8,10 +8,40 @@ $(function() {
     return num !== 1 && num !== 0;
   }
 
-  $('.submit').click(function () {
-    let number = $('.textInput').val();
-    console.log(isPrime(number));
+  function move() {
+    var elem = $("#myBar");
+    elem.css('display', 'block');
+    var width = 0;
+    var id = setInterval(frame, 20);
+
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        showOutput()
+      } else {
+        width++;
+        let temp = width + '%';
+        $(elem).css('width', temp)
+        $(elem).html(width * 1 + '%');
+      }
+    }
+  }
+
+  $('.submit').click(function() {
+    $('.primeOrNot').css('display', 'none');
+    move();
   });
+
+  function showOutput () {
+    $('#myBar').css('display', 'none');
+    $('.primeOrNot').css('display', 'block');
+    if (isPrime($('.textInput').html())){
+        $('.primeOrNot').html('It Is Prime!');
+    }
+    else {
+      $('.primeOrNot').html('It Is Not Prime!');
+    }
+  }
 
 
 
